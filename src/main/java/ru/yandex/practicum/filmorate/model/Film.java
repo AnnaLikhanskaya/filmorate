6b8322d3428f88c8630861449f419f47035ca1fa
AnnaLikhanskaya <a.lihansky@yandex.ru>
2024-07-11 14:49:02 +0300

    Финальный проект 10

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
+@Builder(toBuilder = true)
+
 public class Film {
+    private int id;
+    @NotNull(message = "Название фильма не может быть пустым")
+    @NotEmpty(message = "Не должно быть пустой строки")
+    private String name;
+    @Size(max = 200, message = "Описание фильма не может превышать 200 символов")
+    private String description;
+    @Past(message = "Дата релиза должна быть в прошлом")
+    private LocalDate releaseDate;
+    @Positive(message = "Продолжительность фильма должна быть положительным числом")
+    private int duration;
 }
+
+
