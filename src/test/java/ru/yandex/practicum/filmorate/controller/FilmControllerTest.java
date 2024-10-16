ab1c22ab80e3d14ff0cdba11f59ed482c008224c
AnnaLikhanskaya <a.lihansky@yandex.ru>
2024-10-16 15:32:31 +0300

    Закоментировала старые тесты

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,27 +1,11 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import ru.yandex.practicum.filmorate.adapter.LocalDateAdapter;
-import ru.yandex.practicum.filmorate.model.Film;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
@@ -29,97 +13,97 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class FilmControllerTest {
-
-    private final URI url = URI.create("http://localhost:8080/films");
-    private Film nullname;
-    private Film incorrectDescription;
-    private Film incorrectReleaseDate;
-    private Film negativeDuration;
-    private Film correctFilm;
-    private Film nonexistentId;
-
-    @BeforeEach
-    public void init() {
-        nullname = new Film(null, null, null, "Duis in consequat esse",
-                LocalDate.of(1946, 8, 20), 100L, null, null, null);
-        incorrectDescription = new Film(null, null,
-                "labore nulla", "Пятеро друзей ( комик-группа «Шарло»)," +
-                " приезжают в город Бризуль. Здесь они хотят разыскать господина Огюста Куглова," +
-                " который задолжал им деньги, а именно 20 миллионов. о Куглов, который за время «своего отсутствия»," +
-                " стал кандидатом Коломбани.",
-                LocalDate.of(1946, 8, 20), 100L, null, null, null);
-        incorrectReleaseDate = new Film(null, null, "labore nulla", "Duis in consequat esse",
-                LocalDate.of(1884, 8, 20), 100L, null, null, null);
-        negativeDuration = new Film(null, null, "labore nulla", "Duis in consequat esse",
-                LocalDate.of(1946, 8, 20), -3L, null, null, null);
-        correctFilm = new Film(1, null, "labore nulla", "Duis in consequat esse",
-                LocalDate.of(1946, 8, 20), 100L, null, null, null);
-        nonexistentId = new Film(9999, null, "labore nulla", "Duis in consequat esse",
-                LocalDate.of(1946, 8, 20), -3L, null, null, null);
-    }
-
-    private int postToServer(Film film) throws IOException, InterruptedException {
-        GsonBuilder gsonBuilder = new GsonBuilder()
-                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter());
-        Gson gson = gsonBuilder.create();
-        String userSerialized = gson.toJson(film);
-        HttpClient client = HttpClient.newHttpClient();
-        final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(userSerialized);
-        HttpRequest requestNullLogin = HttpRequest.newBuilder()
-                .uri(url)
-                .header("Content-Type", "application/json")
-                .POST(body).build();
-        HttpResponse<String> responseNullLogin = client.send(requestNullLogin, HttpResponse.BodyHandlers.ofString());
-        return responseNullLogin.statusCode();
-    }
-
-
-    private int putToServer(Film film) throws IOException, InterruptedException {
-        GsonBuilder gsonBuilder = new GsonBuilder()
-                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter());
-        Gson gson = gsonBuilder.create();
-        String userSerialized = gson.toJson(film);
-        HttpClient client = HttpClient.newHttpClient();
-        final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(userSerialized);
-
-        HttpRequest requestNullLogin = HttpRequest.newBuilder()
-                .uri(url)
-                .header("Content-Type", "application/json")
-                .PUT(body).build();
-        HttpResponse<String> responseNullLogin = client.send(requestNullLogin, HttpResponse.BodyHandlers.ofString());
-        return responseNullLogin.statusCode();
-    }
-
-    @Test
-    void postFilms() {
-        assertAll(
-                () -> assertEquals(400, postToServer(nullname),
-                        "Имя не должно быть null, статус 400"),
-                () -> assertEquals(400, postToServer(incorrectDescription),
-                        "Описание не должно привышать 200 символов, статус 400"),
-                () -> assertEquals(400, postToServer(incorrectReleaseDate),
-                        "релиз не может быть раньше 28.12.1895, статус 400"),
-                () -> assertEquals(400, postToServer(negativeDuration),
-                        "продолжительность не может быть отрицательной, статус 400")
-        );
-    }
-
-    @Test
-    void putUsers() throws IOException, InterruptedException {
-        postToServer(correctFilm);
-        assertAll(
-                () -> assertEquals(400, putToServer(nullname),
-                        "Имя не должно быть null, статус 400"),
-                () -> assertEquals(400, putToServer(incorrectDescription),
-                        "Описание не должно привышать 200 символов, статус 400"),
-                () -> assertEquals(400, putToServer(incorrectReleaseDate),
-                        "релиз не может быть раньше 28.12.1895, статус 400"),
-                () -> assertEquals(400, putToServer(negativeDuration),
-                        "продолжительность не может быть отрицательной, статус 400"),
-                () -> assertEquals(400, putToServer(nonexistentId),
-                        "Фильм не существует, статус 404"),
-                () -> assertEquals(400, putToServer(correctFilm),
-                        "Фильм создан верно, статус 200")
-        );
-    }
+//
+//    private final URI url = URI.create("http://localhost:8080/films");
+//    private Film nullname;
+//    private Film incorrectDescription;
+//    private Film incorrectReleaseDate;
+//    private Film negativeDuration;
+//    private Film correctFilm;
+//    private Film nonexistentId;
+//
+//    @BeforeEach
+//    public void init() {
+//        nullname = new Film(null, null, null, "Duis in consequat esse",
+//                LocalDate.of(1946, 8, 20), 100L, null, null, null);
+//        incorrectDescription = new Film(null, null,
+//                "labore nulla", "Пятеро друзей ( комик-группа «Шарло»)," +
+//                " приезжают в город Бризуль. Здесь они хотят разыскать господина Огюста Куглова," +
+//                " который задолжал им деньги, а именно 20 миллионов. о Куглов, который за время «своего отсутствия»," +
+//                " стал кандидатом Коломбани.",
+//                LocalDate.of(1946, 8, 20), 100L, null, null, null);
+//        incorrectReleaseDate = new Film(null, null, "labore nulla", "Duis in consequat esse",
+//                LocalDate.of(1884, 8, 20), 100L, null, null, null);
+//        negativeDuration = new Film(null, null, "labore nulla", "Duis in consequat esse",
+//                LocalDate.of(1946, 8, 20), -3L, null, null, null);
+//        correctFilm = new Film(1, null, "labore nulla", "Duis in consequat esse",
+//                LocalDate.of(1946, 8, 20), 100L, null, null, null);
+//        nonexistentId = new Film(9999, null, "labore nulla", "Duis in consequat esse",
+//                LocalDate.of(1946, 8, 20), -3L, null, null, null);
+//    }
+//
+//    private int postToServer(Film film) throws IOException, InterruptedException {
+//        GsonBuilder gsonBuilder = new GsonBuilder()
+//                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter());
+//        Gson gson = gsonBuilder.create();
+//        String userSerialized = gson.toJson(film);
+//        HttpClient client = HttpClient.newHttpClient();
+//        final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(userSerialized);
+//        HttpRequest requestNullLogin = HttpRequest.newBuilder()
+//                .uri(url)
+//                .header("Content-Type", "application/json")
+//                .POST(body).build();
+//        HttpResponse<String> responseNullLogin = client.send(requestNullLogin, HttpResponse.BodyHandlers.ofString());
+//        return responseNullLogin.statusCode();
+//    }
+//
+//
+//    private int putToServer(Film film) throws IOException, InterruptedException {
+//        GsonBuilder gsonBuilder = new GsonBuilder()
+//                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter());
+//        Gson gson = gsonBuilder.create();
+//        String userSerialized = gson.toJson(film);
+//        HttpClient client = HttpClient.newHttpClient();
+//        final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(userSerialized);
+//
+//        HttpRequest requestNullLogin = HttpRequest.newBuilder()
+//                .uri(url)
+//                .header("Content-Type", "application/json")
+//                .PUT(body).build();
+//        HttpResponse<String> responseNullLogin = client.send(requestNullLogin, HttpResponse.BodyHandlers.ofString());
+//        return responseNullLogin.statusCode();
+//    }
+//
+//    @Test
+//    void postFilms() {
+//        assertAll(
+//                () -> assertEquals(400, postToServer(nullname),
+//                        "Имя не должно быть null, статус 400"),
+//                () -> assertEquals(400, postToServer(incorrectDescription),
+//                        "Описание не должно привышать 200 символов, статус 400"),
+//                () -> assertEquals(400, postToServer(incorrectReleaseDate),
+//                        "релиз не может быть раньше 28.12.1895, статус 400"),
+//                () -> assertEquals(400, postToServer(negativeDuration),
+//                        "продолжительность не может быть отрицательной, статус 400")
+//        );
+//    }
+//
+//    @Test
+//    void putUsers() throws IOException, InterruptedException {
+//        postToServer(correctFilm);
+//        assertAll(
+//                () -> assertEquals(400, putToServer(nullname),
+//                        "Имя не должно быть null, статус 400"),
+//                () -> assertEquals(400, putToServer(incorrectDescription),
+//                        "Описание не должно привышать 200 символов, статус 400"),
+//                () -> assertEquals(400, putToServer(incorrectReleaseDate),
+//                        "релиз не может быть раньше 28.12.1895, статус 400"),
+//                () -> assertEquals(400, putToServer(negativeDuration),
+//                        "продолжительность не может быть отрицательной, статус 400"),
+//                () -> assertEquals(400, putToServer(nonexistentId),
+//                        "Фильм не существует, статус 404"),
+//                () -> assertEquals(400, putToServer(correctFilm),
+//                        "Фильм создан верно, статус 200")
+//        );
+//    }
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,124 +1,108 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import ru.yandex.practicum.filmorate.adapter.LocalDateAdapter;
-import ru.yandex.practicum.filmorate.model.User;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
 @ActiveProfiles("test")
 public class UserControllerTest {
-    private final URI url = URI.create("http://localhost:8080/users");
-    private User nullLogin;
-    private User nullEmail;
-    private User incorrectLogin;
-    private User incorrectBirthday;
-    private User incorrectEmail;
-    private User nullName;
-    private User nonexistentId;
-
-    @BeforeEach
-    public void init() {
-        nullLogin = new User(null, "mail@mail.ru", null, "Имя",
-                LocalDate.of(1946, 8, 20), null);
-        nullEmail = new User(null, null, null, "Имя",
-                LocalDate.of(1946, 8, 20), null);
-        incorrectLogin = new User(1, "mail@mail.ru", "Ло гин", "Имя",
-                LocalDate.of(1946, 8, 20), null);
-        incorrectBirthday = new User(null, "mail@mail.ru", "Логин", "Имя",
-                LocalDate.of(2050, 8, 20), null);
-        incorrectEmail = new User(null, "mailmail.ru",
-                "Логин", "Имя", LocalDate.of(1946, 8, 20), null);
-        nullName = new User(1, "mail@mail.ru",
-                "Логин", "Имя", LocalDate.of(1946, 8, 20), null);
-        nonexistentId = new User(9999, "mail@mail.ru",
-                "Логин", "Имя", LocalDate.of(1946, 8, 20), null);
-    }
-
-    private int postToServer(User user) throws IOException, InterruptedException {
-        GsonBuilder gsonBuilder = new GsonBuilder()
-                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter());
-        Gson gson = gsonBuilder.create();
-
-        String userSerialized = gson.toJson(user);
-        HttpClient client = HttpClient.newHttpClient();
-
-        final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(userSerialized);
-        HttpRequest requestNullLogin = HttpRequest.newBuilder()
-                .uri(url)
-                .header("Content-Type", "application/json")
-                .POST(body).build();
-        HttpResponse<String> responseNullLogin = client.send(requestNullLogin, HttpResponse.BodyHandlers.ofString());
-        return responseNullLogin.statusCode();
-    }
-
-    private int putToServer(User user) throws IOException, InterruptedException {
-        GsonBuilder gsonBuilder = new GsonBuilder()
-                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter());
-        Gson gson = gsonBuilder.create();
-        String userSerialized = gson.toJson(user);
-
-        HttpClient client = HttpClient.newHttpClient();
-        final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(userSerialized);
-        HttpRequest requestNullLogin = HttpRequest.newBuilder()
-                .uri(url)
-                .header("Content-Type", "application/json")
-                .PUT(body).build();
-        HttpResponse<String> responseNullLogin = client.send(requestNullLogin, HttpResponse.BodyHandlers.ofString());
-        return responseNullLogin.statusCode();
-    }
-
-    @Test
-    void postUsers() {
-        assertAll(
-                () -> assertEquals(400, postToServer(nullLogin),
-                        "Логин не должен быть null, статус 400"),
-                () -> assertEquals(400, postToServer(incorrectLogin),
-                        "Логин не должен содержать пробелы, статус 400"),
-                () -> assertEquals(400, postToServer(incorrectEmail),
-                        "Email некоррекный, статус 400"),
-                () -> assertEquals(400, postToServer(nullEmail),
-                        "Email не должен быть null, статус 400"),
-                () -> assertEquals(400, postToServer(incorrectBirthday),
-                        "День рождения не должен быть в будующем, статус 400"),
-                () -> assertEquals(500, postToServer(nullName),
-                        "Имя может быть пустым, статус 200")
-        );
-    }
-
-    @Test
-    void putUsers() throws IOException, InterruptedException {
-        postToServer(nullName);
-        assertAll(
-                () -> assertEquals(400, putToServer(nullLogin),
-                        "Логин не должен быть null, статус 400"),
-                () -> assertEquals(400, putToServer(incorrectLogin),
-                        "Логин не должен содержать пробелы, статус 400"),
-                () -> assertEquals(400, putToServer(incorrectEmail),
-                        "Email некоррекный, статус 400"),
-                () -> assertEquals(400, putToServer(nullEmail),
-                        "Email не должен быть null, статус 400"),
-                () -> assertEquals(400, putToServer(incorrectBirthday),
-                        "День рождения не должен быть в будующем, статус 400"),
-                () -> assertEquals(200, putToServer(nullName),
-                        "Имя может быть пустым, статус 200"),
-                () -> assertEquals(404, putToServer(nonexistentId),
-                        "Такого пользователя не существует, статус 404")
-        );
-    }
+//    private final URI url = URI.create("http://localhost:8080/users");
+//    private User nullLogin;
+//    private User nullEmail;
+//    private User incorrectLogin;
+//    private User incorrectBirthday;
+//    private User incorrectEmail;
+//    private User nullName;
+//    private User nonexistentId;
+//
+//    @BeforeEach
+//    public void init() {
+//        nullLogin = new User(null, "mail@mail.ru", null, "Имя",
+//                LocalDate.of(1946, 8, 20), null);
+//        nullEmail = new User(null, null, null, "Имя",
+//                LocalDate.of(1946, 8, 20), null);
+//        incorrectLogin = new User(1, "mail@mail.ru", "Ло гин", "Имя",
+//                LocalDate.of(1946, 8, 20), null);
+//        incorrectBirthday = new User(null, "mail@mail.ru", "Логин", "Имя",
+//                LocalDate.of(2050, 8, 20), null);
+//        incorrectEmail = new User(null, "mailmail.ru",
+//                "Логин", "Имя", LocalDate.of(1946, 8, 20), null);
+//        nullName = new User(1, "mail@mail.ru",
+//                "Логин", "Имя", LocalDate.of(1946, 8, 20), null);
+//        nonexistentId = new User(9999, "mail@mail.ru",
+//                "Логин", "Имя", LocalDate.of(1946, 8, 20), null);
+//    }
+//
+//    private int postToServer(User user) throws IOException, InterruptedException {
+//        GsonBuilder gsonBuilder = new GsonBuilder()
+//                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter());
+//        Gson gson = gsonBuilder.create();
+//
+//        String userSerialized = gson.toJson(user);
+//        HttpClient client = HttpClient.newHttpClient();
+//
+//        final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(userSerialized);
+//        HttpRequest requestNullLogin = HttpRequest.newBuilder()
+//                .uri(url)
+//                .header("Content-Type", "application/json")
+//                .POST(body).build();
+//        HttpResponse<String> responseNullLogin = client.send(requestNullLogin, HttpResponse.BodyHandlers.ofString());
+//        return responseNullLogin.statusCode();
+//    }
+//
+//    private int putToServer(User user) throws IOException, InterruptedException {
+//        GsonBuilder gsonBuilder = new GsonBuilder()
+//                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter());
+//        Gson gson = gsonBuilder.create();
+//        String userSerialized = gson.toJson(user);
+//
+//        HttpClient client = HttpClient.newHttpClient();
+//        final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(userSerialized);
+//        HttpRequest requestNullLogin = HttpRequest.newBuilder()
+//                .uri(url)
+//                .header("Content-Type", "application/json")
+//                .PUT(body).build();
+//        HttpResponse<String> responseNullLogin = client.send(requestNullLogin, HttpResponse.BodyHandlers.ofString());
+//        return responseNullLogin.statusCode();
+//    }
+//
+//    @Test
+//    void postUsers() {
+//        assertAll(
+//                () -> assertEquals(400, postToServer(nullLogin),
+//                        "Логин не должен быть null, статус 400"),
+//                () -> assertEquals(400, postToServer(incorrectLogin),
+//                        "Логин не должен содержать пробелы, статус 400"),
+//                () -> assertEquals(400, postToServer(incorrectEmail),
+//                        "Email некоррекный, статус 400"),
+//                () -> assertEquals(400, postToServer(nullEmail),
+//                        "Email не должен быть null, статус 400"),
+//                () -> assertEquals(400, postToServer(incorrectBirthday),
+//                        "День рождения не должен быть в будующем, статус 400"),
+//                () -> assertEquals(500, postToServer(nullName),
+//                        "Имя может быть пустым, статус 200")
+//        );
+//    }
+//
+//    @Test
+//    void putUsers() throws IOException, InterruptedException {
+//        postToServer(nullName);
+//        assertAll(
+//                () -> assertEquals(400, putToServer(nullLogin),
+//                        "Логин не должен быть null, статус 400"),
+//                () -> assertEquals(400, putToServer(incorrectLogin),
+//                        "Логин не должен содержать пробелы, статус 400"),
+//                () -> assertEquals(400, putToServer(incorrectEmail),
+//                        "Email некоррекный, статус 400"),
+//                () -> assertEquals(400, putToServer(nullEmail),
+//                        "Email не должен быть null, статус 400"),
+//                () -> assertEquals(400, putToServer(incorrectBirthday),
+//                        "День рождения не должен быть в будующем, статус 400"),
+//                () -> assertEquals(200, putToServer(nullName),
+//                        "Имя может быть пустым, статус 200"),
+//                () -> assertEquals(404, putToServer(nonexistentId),
+//                        "Такого пользователя не существует, статус 404")
+//        );
+//    }
 }
